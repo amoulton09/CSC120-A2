0601373e8e812aaf1c04bf4238aba25395b3b6a1
amoulton09 <amoulton@smith.edu>
2026-02-10 14:26:08 -0500

    Adding attributes to resale_shop

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,20 @@
 class ResaleShop:
-
     # What attributes will it need?
+    inventory: list = []
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, inventory: list):
+        self.inventory = inventory
+        #pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
+    #def buy_computer(self, item: str)
+
+
+def main():
+    my_store:ResaleShop = ResaleShop(["computer1", "computer2"])
+    print(my_store.inventory)
+
+if __name__ == "__main__":
+    main()
