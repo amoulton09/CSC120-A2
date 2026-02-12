46799af3d924289e541f14d0b80168c2159272d6
amoulton09 <amoulton@smith.edu>
2026-02-11 20:17:15 -0500

    Added 'refurbish' method to resale shop

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,3 +1,4 @@
+import computer
 from computer import Computer
 
 class ResaleShop:
@@ -19,18 +20,35 @@ class ResaleShop:
         self.inventory.remove(computer)
         return(self.inventory)
 
-    #def refurbish_computer(self, computer: Computer) #change os and then price based on year
+    def refurbish_computer(self, computer: Computer, os: str): #change os and then price based on year
+        if computer not in self.inventory:
+            print("No Computer to refurbish!!")
+        else:
+            computer.update_os(os)
+            if computer.year_made >= 2020:
+                computer.update_price(1000)
+                return(computer)
+            elif computer.year_made >= 2010:
+                computer.update_price(550)
+                return(computer)
+            elif computer.year_made >= 2000:
+                computer.update_price(200)
+                return(computer)
+
+        
 
 def main():
     computer1:Computer = Computer("Windows", "MyOs", 2014, 200)
-    computer2:Computer = Computer("Mac", "MacOs", 2021, 1000)
+    computer2:Computer = Computer("Mac", "MacOs", 2000, 1000)
     my_store:ResaleShop = ResaleShop([computer1, computer2])
     print(my_store.inventory)
     computer3 = Computer("Cool guy", "lala", 2000, 100)
     my_store.buy_computer(computer3)
     print(my_store.inventory)
-    my_store.sell_computer(computer3)
+    my_store.sell_computer(computer1)
     print(my_store.inventory)
-
+    my_store.refurbish_computer(computer1, "My_os3")
+    print(my_store.inventory)
+  
 if __name__ == "__main__":
     main()
